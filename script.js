ab41c0b73ddfd83b08266229be0cf2725a833ff0
Alicamara20 <ali.camara1955@gmail.com>
2025-04-18 20:06:27 +0100

    update script.ts

--- script.js
+++ script.js
@@ -1,107 +1,159 @@
-var questionContainer = document.querySelector('.question_list');
-var wrongAnswersContainer = document.querySelector('.wrong_answers');
-var pass_failParagraph = document.querySelector('.pass_fail');
-var result_section = document.getElementById("result");
-var correctAnswers = ['B', 'D', 'A', 'A', 'C', 'A', 'B', 'A', 'C', 'D',
+const questionContainer = document.querySelector('.question_list');
+const wrongAnswersContainer = document.querySelector('.wrong_answers');
+const pass_failParagraph = document.querySelector('.pass_fail');
+const result_section = document.getElementById("result") 
+const correctAnswers = ['B', 'D', 'A', 'A', 'C', 'A', 'B', 'A', 'C', 'D', 
     'B', 'C', 'D', 'A', 'D', 'C', 'C', 'B', 'D', 'A'
-];
-var wrongAnswers = [];
-var stundentAnswers = [];
-var numOfCorrectAnswers = 0;
-var numOfWrongAnswers = 0;
-var inputLabel = 1;
-var options = ['A', 'B', 'C', 'D'];
-var _loop_1 = function (i) {
-    var listItem = document.createElement('li');
-    listItem.className = 'inputs-list';
-    listItem.id = "listItem_".concat(i);
-    var inputsContainer = document.createElement('div');
-    inputsContainer.className = 'input_containers__container';
-    options.map(function (option) {
-        var inputId = inputLabel++;
-        var divElement = document.createElement('div');
+]
+
+
+
+
+let wrongAnswers = [];
+let stundentAnswers = [];
+let numOfCorrectAnswers = 0;
+let numOfWrongAnswers = 0;
+let inputLabel = 1;
+let options = ['A', 'B', 'C', 'D']
+ 
+for( let i = 0; i < correctAnswers.length; i++){
+    let listItem = document.createElement('li');
+    listItem.className = 'inputs-list'
+    listItem.id = `listItem_${i}`
+    let inputsContainer = document.createElement('div');
+    inputsContainer.className = 'input_containers__container'
+    options.map( (option) => {
+        let inputId = inputLabel++;
+        let divElement  = document.createElement('div');
         divElement.className = 'input-container';
-        divElement.innerHTML = "\n            <label for= radio_btn_".concat(inputId, " class=\"option_label\">").concat(option, "</label>\n            <input type=\"radio\" class=\"option_btn\" value= ").concat(option, " name= \"question_").concat(i, "\" id= radio_btn_").concat(inputId, ">\n        ");
+      
+        divElement.innerHTML = `
+            <label for= radio_btn_${inputId} class="option_label">${option}</label>
+            <input type="radio" class="option_btn" value= ${option} name= "question_${i}" id= radio_btn_${inputId}>
+        `
         inputsContainer.appendChild(divElement);
-        var optionBtn = inputsContainer.querySelector("#radio_btn_".concat(inputId));
-        optionBtn.addEventListener("change", function () {
-            if (stundentAnswers.length === 0) {
+        const optionBtn = inputsContainer.querySelector(`#radio_btn_${inputId}`) 
+        optionBtn.addEventListener("change", () => {
+            if(stundentAnswers.length === 0){
                 stundentAnswers.push({
-                    id: i,
+                    id: i ,
                     chosenOption: option
-                });
+                }) 
             }
-            if (stundentAnswers[i]) {
-                var choosenOption = stundentAnswers[i].chosenOption;
-                if (option !== choosenOption)
-                    stundentAnswers[i].chosenOption = option;
+                
+            if(stundentAnswers[i]){
+                let choosenOption = stundentAnswers[i].chosenOption
+                if(option !== choosenOption) stundentAnswers[i].chosenOption = option;    
             }
-            else {
+            else{
                 stundentAnswers.push({
-                    id: i,
+                    id: i ,
                     chosenOption: option
-                });
+                })
             }
-        });
-    });
+            console.log(stundentAnswers);  
+        })
+        
+    })
+
     listItem.appendChild(inputsContainer);
     questionContainer.appendChild(listItem);
-};
-for (var i = 0; i < correctAnswers.length; i++) {
-    _loop_1(i);
+    
 }
-function scrollToElement(element) {
-    if (element) {
-        element.style.display = 'block';
+
+
+
+
+function scrollToElement(element){
+    if(element){
+        element.style.display = 'block'
         element.scrollIntoView({
             behavior: 'smooth',
             inline: "center",
             block: "nearest"
-        });
+        })
     }
+    
 }
-function getWrongAnswers() {
-    var sortedWrongAnswers = wrongAnswers.sort(function (a, b) { return a.question - b.question; });
-    sortedWrongAnswers.map(function (w) {
-        var wrongAnswer = document.createElement('p');
+
+
+function getWrongAnswers(){
+    wrongAnswersContainer.innerHTML = "";
+    let sortedWrongAnswers = wrongAnswers.sort((a, b) => a.question - b.question);
+    sortedWrongAnswers.map( w => {
+        let wrongAnswer = document.createElement('p');
         wrongAnswer.className = 'wrong_answer';
-        wrongAnswer.innerText =
-            "".concat(w.question, ". You chose ").concat(w.wrongAnswer, ", The correct answer is ").concat(w.correctAnswer);
+        wrongAnswer.innerText = 
+            `${w.question}. You chose ${w.wrongAnswer}, The correct answer is ${w.correctAnswer}`
         wrongAnswersContainer.appendChild(wrongAnswer);
-    });
-    console.log("sorted wrong answers:");
-    console.log(sortedWrongAnswers);
+        
+    })
+        
+    
 }
-function getScore() {
-    for (var i = 0; i < stundentAnswers.length; i++) {
-        var currentAnswer = stundentAnswers[i];
-        if (stundentAnswers[i].chosenOption === correctAnswers[currentAnswer.id])
-            numOfCorrectAnswers++;
-        else {
-            wrongAnswers.push({
-                question: currentAnswer.id + 1,
-                wrongAnswer: stundentAnswers[i].chosenOption.toString(),
-                correctAnswer: correctAnswers[currentAnswer.id]
-            });
-        }
+
+
+function getScore(){
+
+    if(stundentAnswers.length < 20){
+        alert('All questions must be answered');
+        return;
     }
-    if (wrongAnswers.length === 0) {
-        var unAnsweredQuestions = correctAnswers.length - stundentAnswers.length;
-        if (unAnsweredQuestions > 0) {
-            wrongAnswersContainer.innerHTML = "\n                <p class = 'wrong_answer'>\n                    No wrong answers, ".concat(unAnsweredQuestions, " question(s) are not answered\n                </p>\n            ");
-        }
-        else
-            wrongAnswersContainer.innerHTML = "<p class = 'wrong_answer'>All answers are correct</p>";
+
+    for(let i = 0; i < stundentAnswers.length; i++){
+        let currentAnswer  = stundentAnswers[i];
+        if(stundentAnswers[i].chosenOption === correctAnswers[currentAnswer.id]) numOfCorrectAnswers ++
+        else{
+            
+                wrongAnswers.push({
+                    question: currentAnswer.id + 1,
+                    wrongAnswer: stundentAnswers[i].chosenOption.toString(),
+                    correctAnswer: correctAnswers[currentAnswer.id]
+                });
+        }     
+            
     }
-    var resultContainer = document.getElementById('resultSection');
+
+    if(wrongAnswers.length === 0){
+        let unAnsweredQuestions = correctAnswers.length - stundentAnswers.length;
+        if(unAnsweredQuestions > 0){
+            wrongAnswersContainer.innerHTML = `
+                <p class = 'wrong_answer'>
+                    No wrong answers, ${unAnsweredQuestions} question(s) are not answered
+                </p>
+            `
+        }
+
+        else wrongAnswersContainer.innerHTML = `<p class = 'wrong_answer'>All answers are correct</p>`
+    }
+
+    let resultContainer = document.getElementById('resultSection') ;
     scrollToElement(resultContainer);
-    result_section.innerText = " Stundent's score: ".concat(numOfCorrectAnswers, " out of  ").concat(correctAnswers.length);
-    pass_failParagraph.innerText = numOfCorrectAnswers >= 15 ? "You Pass" : "You Fail";
-    pass_failParagraph.className = numOfCorrectAnswers >= 15 ? "pass_fail pass" : "pass_fail fail";
+    result_section.innerText = ` Stundent's score: ${numOfCorrectAnswers} out of  ${correctAnswers.length}`
+    pass_failParagraph.innerText = numOfCorrectAnswers >= 15 ? `You Pass` : `You Fail`;
+    pass_failParagraph.className = numOfCorrectAnswers >= 15 ? `pass_fail pass` : `pass_fail fail`;
     getWrongAnswers();
+
     console.log(stundentAnswers);
     console.log(wrongAnswers);
-    console.log("num of wrong asnwers: " + wrongAnswers.length);
-    var examForm = document.getElementById('exam_form');
+    console.log( "num of wrong asnwers: " + wrongAnswers.length);
+    let examForm = document.getElementById('exam_form') ;
     examForm.reset();
+    reset();
+    
 }
+
+function reset(){
+    wrongAnswers = [];
+    stundentAnswers = [];
+    numOfCorrectAnswers = 0;
+    numOfWrongAnswers = 0;
+}
+
+
+
+
+    
+
+
+
